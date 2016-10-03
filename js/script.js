c088c6646f112639491d912ff16e80c9f0fed954
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-10-03 19:26:55 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+var weather = function() {
+
+    var activeClass = function() {
+        var listItems = document.querySelectorAll('nav ul li');
+        function handleClick() {
+            for (var i = 0; i < listItems.length; i++) {
+                listItems[i].classList.remove('active');
+            }
+            this.classList.add('active');
+        }
+        for (var i = 0; i < listItems.length; i++) {
+            listItems[i].addEventListener('click', handleClick);
+        }
+    }();
+
+    var myRequest = function() {
+        var request = new XMLHttpRequest();
+        request.open('GET', 'api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '');
+        request.send();
+        console.log(request);
+    };
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position)     {
+            var myRequest = function() {
+                var request = new XMLHttpRequest();
+                request.open('GET', 'api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '');
+                request.onreadystatechange = function() {
+                    if( (request.status === 200) && (request.readyState === 4) ) {
+                        console.log(request);
+                    }
+                };
+                request.send();
+            }();
+        });
+    }
+
+
+    var sunShowerIcon = document.querySelector('#sun-shower').innerHTML;
+    var iconElement = document.getElementById('current-icon');
+    iconElement.innerHTML = sunShowerIcon;
+
+}();
