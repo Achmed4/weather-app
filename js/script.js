a23a71454cfb0e769964d0375501b8bdb320d729
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-10-09 13:39:13 +0200

    Making a CROS request

--- js/script.js
+++ js/script.js
@@ -13,21 +13,54 @@ var weather = function() {
         }
     }();
 
+    function createCORSRequest(method, url) {
+        var xhr = new XMLHttpRequest();
+        if ("withCredentials" in xhr) {
+
+            // Check if the XMLHttpRequest object has a "withCredentials" property.
+            // "withCredentials" only exists on XMLHTTPRequest2 objects.
+            xhr.open(method, url, true);
+
+        } else if (typeof XDomainRequest != "undefined") {
+
+            // Otherwise, check if XDomainRequest.
+            // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+            xhr = new XDomainRequest();
+            xhr.open(method, url);
+
+        } else {
+
+            // Otherwise, CORS is not supported by the browser.
+            xhr = null;
+
+        }
+        return xhr;
+    }
+
+    // var xhr = createCORSRequest('GET', url);
+    // if (!xhr) {
+    //     throw new Error('CORS not supported');
+    // }
+
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position)     {
             var myRequest = function() {
-                var request = new XMLHttpRequest();
-                request.open('GET',  'https://api.apixu.com/v1/current.json?key=a13aa5f20b72400c8d9235114160710&q=' + position.coords.latitude + ',' + position.coords.longitude);
-                request.onreadystatechange = function() {
+                // var request = new XMLHttpRequest();
+                var url = 'https://api.darksky.net/forecast/cd80205e6d9b5496aca91ffefffa3a83/' + position.coords.latitude + ',' + position.coords.longitude;
+                var xhr = createCORSRequest('GET', url);
+                if (!xhr) {
+                    throw new Error('CORS not supported');
+                }
+                // request.open('GET',  'https://api.darksky.net/forecast/cd80205e6d9b5496aca91ffefffa3a83/' + position.coords.latitude + ',' + position.coords.longitude);
+                xhr.onreadystatechange = function() {
                     if( (request.status === 200) && (request.readyState === 4) ) {
-                        console.log(request);
+                        console.log(xhr);
                     }
                 };
-                request.send();
+                xhr.send();
             }();
         });
     }
-
 
     var sunShowerIcon = document.querySelector('#sun-shower').innerHTML;
     var iconElement = document.getElementById('current-icon');
