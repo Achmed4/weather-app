d237009543e5ba43ee1ef94b265bb9635bcae1dc
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-10-09 19:23:29 +0200

    Added high and low temp

--- js/script.js
+++ js/script.js
@@ -22,16 +22,29 @@ var weather = function() {
             xhr.open('GET',  '//api.wunderground.com/api/ddfbc22639b02032/forecast10day/q/' + position.coords.latitude + ',' + position.coords.longitude + '.json');
             xhr.onreadystatechange = function() {
                 if( (xhr.status === 200) && (xhr.readyState === 4) ) {
+
+                    //Information
                     var response = JSON.parse(xhr.responseText);
+                    var currentTime = response.forecast.txt_forecast.date;
                     var hour = response.forecast.simpleforecast.forecastday[0].date.hour;
                     var min = response.forecast.simpleforecast.forecastday[0].date.min;
                     var ampm = response.forecast.simpleforecast.forecastday[0].date.ampm;
                     var month = response.forecast.simpleforecast.forecastday[0].date.monthname;
                     var day = response.forecast.simpleforecast.forecastday[0].date.day;
+                    var highTempC = response.forecast.simpleforecast.forecastday[0].high.celsius;
+                    var highTempF = response.forecast.simpleforecast.forecastday[0].high.fahrenheit;
+                    var lowTempC = response.forecast.simpleforecast.forecastday[0].low.celsius;
+                    var lowTempF = response.forecast.simpleforecast.forecastday[0].low.fahrenheit;
+                    //Elements
                     var topTimeElement = document.querySelector('main header section div time');
                     var bottomTimeElement = document.querySelector('main article section time');
-                    topTimeElement.innerHTML = hour + ':' + min + ' ' + ampm;
+                    var highTemp = document.getElementById('highTemp');
+                    var lowTemp = document.getElementById('lowTemp');
+                    topTimeElement.innerHTML = currentTime;
                     bottomTimeElement.innerHTML = month + ' ' + day;
+                    highTemp.innerHTML = highTempC;
+                    lowTemp.innerHTML = lowTempC;
+
                 }
             };
             xhr.send();
@@ -39,7 +52,7 @@ var weather = function() {
         });
     }
 
-    var sunShowerIcon = document.querySelector('#sun-shower').innerHTML;
+    var sunShowerIcon = document.querySelector('#clear').innerHTML;
     var iconElement = document.getElementById('current-icon');
     iconElement.innerHTML = sunShowerIcon;
 
