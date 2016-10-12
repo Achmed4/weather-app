7bf136bd442566190575c260a4bcf936e2d8588f
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-10-12 15:07:46 +0200

    Changing images paths again

--- js/script.js
+++ js/script.js
@@ -136,7 +136,7 @@ var weather = function() {
                         var headerEl = document.querySelector('main header');
                         var blurEl = document.querySelector('.blur');
                         headerEl.style.backgroundImage = 'url(http://achmed4.github.io/img/' + xhrIcon + '.jpg)';
-                        blurEl.style.backgroundImage = 'url(url(http://achmed4.github.io/img/' + xhrIcon + '.jpg)';
+                        blurEl.style.backgroundImage = 'url(http://achmed4.github.io/img/' + xhrIcon + '.jpg)';
 
                         for(var i = 0; i < ourIcons.length; i++) {
                             if( ourIcons[i].getAttribute('id') === xhrIcon ) {
