14bb186337796d82369cf217029ba180eb90f56a
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-10-16 16:16:55 +0200

    Toggle temps of the rest of the week between C & F again

--- js/script.js
+++ js/script.js
@@ -194,10 +194,12 @@ var weather = function() {
                         var temps = document.querySelectorAll('#rest-of-theweek span');
                         var toggleTemps = function() {
 
-                            if( temps[h].innerHTML == (tempsC + '<sup>&#x2218;</sup>C') ) {
-                                temps[h].innerHTML = tempsF + '<sup>&#x2218;</sup>F';
-                            } else if( temps[h].innerHTML == (tempsF + '<sup>&#x2218;</sup>F') ) {
-                                temps[h].innerHTML = tempsC + '<sup>&#x2218;</sup>C';
+                            for(var b = 0; b < temps.length; b++) {
+                                if( temps[b].innerHTML == (tempsC + '<sup>&#x2218;</sup>C') ) {
+                                    temps[b].innerHTML = tempsF + '<sup>&#x2218;</sup>F';
+                                } else if( temps[h].innerHTML == (tempsF + '<sup>&#x2218;</sup>F') ) {
+                                    temps[b].innerHTML = tempsC + '<sup>&#x2218;</sup>C';
+                                }
                             }
 
                         };
