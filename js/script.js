8d7591720b723d53c67bd71123f5aa48df675f9f
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-10-16 16:53:09 +0200

    Toggle temps of the rest of the week between C & F again

--- js/script.js
+++ js/script.js
@@ -191,21 +191,30 @@ var weather = function() {
                             update += '</li>';
                             //Toggle temps between C & F
                             temps = document.querySelectorAll('#rest-of-theweek span');
-                            toggleTemps = function() {
+                            // toggleTemps = function() {
+                            //
+                            //     if( temps[b].innerHTML == (tempsC + '<sup>&#x2218;</sup>C') ) {
+                            //         temps[b].innerHTML = tempsF + '<sup>&#x2218;</sup>F';
+                            //     } else if( temps[h].innerHTML == (tempsF + '<sup>&#x2218;</sup>F') ) {
+                            //         temps[b].innerHTML = tempsC + '<sup>&#x2218;</sup>C';
+                            //     }
+                            //
+                            // };
+                            for(var h = 0; h < temps.length; h++) {
+                                temps[h].addEventListener("click", function() {
 
-                                for(var b = 0; b < temps.length; b++) {
-                                    if( temps[b].innerHTML == (tempsC + '<sup>&#x2218;</sup>C') ) {
-                                        temps[b].innerHTML = tempsF + '<sup>&#x2218;</sup>F';
+                                    if( temps[h].innerHTML == (tempsC + '<sup>&#x2218;</sup>C') ) {
+                                        temps[h].innerHTML = tempsF + '<sup>&#x2218;</sup>F';
                                     } else if( temps[h].innerHTML == (tempsF + '<sup>&#x2218;</sup>F') ) {
-                                        temps[b].innerHTML = tempsC + '<sup>&#x2218;</sup>C';
+                                        temps[h].innerHTML = tempsC + '<sup>&#x2218;</sup>C';
                                     }
-                                }
 
-                            };
+                                });
+                            }
                         }
                         ul.innerHTML = update;
 
-                        // //Toggle temps between C & F
+                        //Toggle temps between C & F
                         // temps = document.querySelectorAll('#rest-of-theweek span');
                         // toggleTemps = function() {
                         //
@@ -218,10 +227,9 @@ var weather = function() {
                         //     }
                         //
                         // };
-                        for(var h = 0; h < temps.length; h++) {
-                            temps[h].addEventListener("click", toggleTemps);
-                        }
-                        // temps.addEventListener("click", toggleTemps);
+                        // for(var h = 0; h < temps.length; h++) {
+                        //     temps[h].addEventListener("click", toggleTemps);
+                        // }
 
                     }
                 };
